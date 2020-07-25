986cd72a4e0b36029b5c3231dcecfc08646c0315
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-25 17:57:26 +0100

    Changes in css and js updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-const inputs = document.querySelectorAll(".controls input");
+{
+    function handleUpdate() {
+        const suffix = this.dataset.sizing || "";
+        document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    }
+    const bindEventListeners = () => {
+        const inputs = document.querySelectorAll(".controls input");
+        inputs.forEach(input => input.addEventListener("change", handleUpdate));
+        inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+    }
 
-function handleUpdate() {
-    const suffix = this.dataset.sizing || "";
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const init = () => {
+        bindEventListeners();
+    }
+    init();
 }
-inputs.forEach(input => input.addEventListener("change", handleUpdate));
-inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
